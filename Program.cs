3d8157e070ef947cd67774afddc8363ff754e76d
HugoRossetti <hugor.rossetti@gmail.com>
2025-01-06 20:31:57 -0400

    mudando a logica da escolha das opções da calculadora

--- Program.cs
+++ Program.cs
@@ -1,84 +1,105 @@
 ﻿using calculadora_de_terminal.Models;
 
 Calculadora calc = new Calculadora();
+Boolean exibirMenu = true;
 
 Console.Clear();
-Console.WriteLine("Bem vindos(as), a calculadora simple de Terminal (precione ENTER para começar!)");
+Console.WriteLine("Bem vindos(as), a Calculadora de Terminal Simples (precione ENTER para começar!)");
 Console.ReadLine();
 
-Console.Clear();
-Console.WriteLine("Escolha a operação desejada: ");
-Console.WriteLine("1 - Soma");
-Console.WriteLine("2 - Subtração");
-Console.WriteLine("3 - Multiplicação");
-Console.WriteLine("4 - Potenciação");
-Console.WriteLine("5 - Seno");
-Console.WriteLine("6 - Coseno");
-Console.WriteLine("7 - Tangente");
-Console.WriteLine("8 - Raiz Quadrada");
-
-
-string opcao = Console.ReadLine().ToUpper();
-Console.Clear();
-
-if (opcao == "5" || opcao == "6" || opcao == "7")
+while (exibirMenu)
 {
-
-  Console.WriteLine("Digite o angulo: ");
-  double angulo = Convert.ToDouble(Console.ReadLine());
-
-  switch (opcao)
-  {
-    case "5":
-      calc.Seno(angulo);
-      break;
-    case "6":
-      calc.Coseno(angulo);
-      break;
-    case "7":
-      calc.Tangente(angulo);
-      break;
-  }
-
-}
-else if (opcao == "8")
-{
-
-  Console.WriteLine("Digite o valor para saber a Raiz: ");
-  double raiz = Convert.ToDouble(Console.ReadLine());
-
-  calc.RaizQuadrada(raiz);
-
-}
-else
-{
-
-  Console.WriteLine("Digite o primeiro Numero: ");
-  double a = Convert.ToDouble(Console.ReadLine());
 
   Console.Clear();
-  Console.WriteLine("Digite o segundo Numero: ");
-  double b = Convert.ToDouble(Console.ReadLine());
+  Console.WriteLine("Escolha a operação desejada: ");
+  Console.WriteLine("1 - Soma");
+  Console.WriteLine("2 - Subtração");
+  Console.WriteLine("3 - Multiplicação");
+  Console.WriteLine("4 - Potenciação");
+  Console.WriteLine("5 - Seno");
+  Console.WriteLine("6 - Coseno");
+  Console.WriteLine("7 - Tangente");
+  Console.WriteLine("8 - Raiz Quadrada");
+  Console.WriteLine("9 - Sair");
 
-  switch (opcao)
+
+  string opcao = Console.ReadLine().ToUpper();
+  Console.Clear();
+
+  if (opcao == "5" || opcao == "6" || opcao == "7")
   {
-    case "1":
-      calc.Somar(a, b);
-      break;
-    case "2":
-      calc.Subtrair(a, b);
-      break;
-    case "3":
-      calc.Dividir(a, b);
-      break;
-    case "4":
-      calc.Multiplicar(a, b);
-      break;
-    case "5":
-      calc.Potencia(a, b);
-      break;
-    default:
-      Console.WriteLine("Opção Inválida");
-      break;
+
+    Console.WriteLine("Digite o angulo: ");
+    double angulo = Convert.ToDouble(Console.ReadLine());
+
+    switch (opcao)
+    {
+      case "5":
+        calc.Seno(angulo);
+        Console.ReadLine();
+        break;
+      case "6":
+        calc.Coseno(angulo);
+        Console.ReadLine();
+        break;
+      case "7":
+        calc.Tangente(angulo);
+        Console.ReadLine();
+        break;
+    }
+
+  }
+  else if (opcao == "8")
+  {
+
+    Console.WriteLine("Digite o valor para saber a Raiz: ");
+    double raiz = Convert.ToDouble(Console.ReadLine());
+
+    calc.RaizQuadrada(raiz);
+    Console.ReadLine();
+
+  }
+  else if (opcao == "9")
+  {
+    exibirMenu = false;
+  }
+  else
+  {
+
+    Console.WriteLine("Digite o primeiro Numero: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+
+    Console.Clear();
+    Console.WriteLine("Digite o segundo Numero: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+
+    switch (opcao)
+    {
+      case "1":
+        calc.Somar(a, b);
+        Console.ReadLine();
+        break;
+      case "2":
+        calc.Subtrair(a, b);
+        Console.ReadLine();
+        break;
+      case "3":
+        calc.Dividir(a, b);
+        Console.ReadLine();
+        break;
+      case "4":
+        calc.Multiplicar(a, b);
+        Console.ReadLine();
+        break;
+      case "5":
+        calc.Potencia(a, b);
+        Console.ReadLine();
+        break;
+      default:
+        Console.WriteLine("Opção Inválida");
+        break;
+    }
   }
 }
+
+Console.WriteLine("Encerrando...");
